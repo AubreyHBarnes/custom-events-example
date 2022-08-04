ce84af2d06a951f2d2e21d1409a20d400c1aa699
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2022-08-03 23:32:24 -0500

    attempt to get functions to detect API_SECRET

--- functions/token-hider/token-hider.js
+++ functions/token-hider/token-hider.js
@@ -11,7 +11,7 @@ const handler = async function (event) {
 
   // TODO: customize your URL and API keys set in the Netlify Dashboard
   // this is secret too, your frontend won't see this
-
+  const API_SECRET = process.env.API_SECRET
   const URL = `https://www.themealdb.com/api/json/v2/${API_SECRET}/randomselection.php`
 
   console.log('Constructed URL is ...', URL)
